dd04d94fd5a9548f5287521490e596311635d781
Nathan <33833899+nate8523@users.noreply.github.com>
2024-12-19 20:29:20 +0000

    Update index.js

--- web-app/pages/index.js
+++ web-app/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import TemplateCard from '../components/TemplateCard';
-import '../styles/styles.css'; // Import the new CSS file for the dark theme
 
 const templates = [
   {
@@ -8,30 +7,26 @@ const templates = [
     description: "Deploy a test resource group in Azure.",
     deployUrl: "https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fnate8523%2FAzureDeployHub%2Frefs%2Fheads%2Fmain%2Fdeployment-templates%2Fdummy%2Ftest-template.json"
   },
-  // Add more templates here
 ];
 
 export default function Home() {
-  return (
-    <div className="container">
-      <header className="text-center py-5">
-        <h1>AzureDeployHub</h1>
-        <p>Your one-stop shop for Azure deployments</p>
-      </header>
-      
-      <main>
-        <div className="row">
-          {templates.map((template) => (
-            <div className="col-md-4 mb-4" key={template.name}>
-              <TemplateCard
-                name={template.name}
-                description={template.description}
-                deployUrl={template.deployUrl}
-              />
+    return (
+        <div className="container">
+            <header className="text-center my-5">
+                <h1>AzureDeployHub</h1>
+                <p>One-click deployment templates for Azure</p>
+            </header>
+            <div className="row">
+                {templates.map((template) => (
+                    <div className="col-md-4" key={template.name}>
+                        <TemplateCard
+                            name={template.name}
+                            description={template.description}
+                            deployUrl={template.deployUrl}
+                        />
+                    </div>
+                ))}
             </div>
-          ))}
         </div>
-      </main>
-    </div>
-  );
+    );
 }
