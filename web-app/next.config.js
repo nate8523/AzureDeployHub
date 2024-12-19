2d22c331760163829d5c27dc2a5bb1d443922193
Nathan <33833899+nate8523@users.noreply.github.com>
2024-12-19 20:03:37 +0000

    Update next.config.js

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Ensures Next.js generates static files for export
-  distDir: 'out'    // Forces Next.js to use the 'out' directory
+  output: 'standalone', // Use "standalone" if you're optimizing for serverless or containerized deployments
+  distDir: 'out',       // Use "out" as the build output directory
 };
 
 module.exports = nextConfig;
