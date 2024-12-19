ad2e055c40435b088bb7566a413b7c3933fcac24
Nathan <33833899+nate8523@users.noreply.github.com>
2024-12-19 16:22:14 +0000

    Update index.js

--- web-app/pages/index.js
+++ web-app/pages/index.js
@@ -5,7 +5,7 @@ const templates = [
   {
     name: "Test Resource Group",
     description: "Deploy a test resource group in Azure.",
-    deployUrl: "https://portal.azure.com/#create/Microsoft.Template/uri/https://raw.githubusercontent.com/nate8523/AzureDeployHub/main/deployment-templates/dummy/test-template.json"
+    deployUrl: "https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fnate8523%2FAzureDeployHub%2Frefs%2Fheads%2Fmain%2Fdeployment-templates%2Fdummy%2Ftest-template.json"
   }
 ];
 
