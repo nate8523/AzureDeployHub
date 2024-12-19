c52a6a9e41f279afdec05805108165dafea59514
Nathan <33833899+nate8523@users.noreply.github.com>
2024-12-19 18:51:02 +0000

    Update index.js

--- web-app/pages/index.js
+++ web-app/pages/index.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import TemplateCard from '../components/TemplateCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const templates = [
   {
     name: "Test Resource Group",
     description: "Deploy a test resource group in Azure.",
     deployUrl: "https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fnate8523%2FAzureDeployHub%2Frefs%2Fheads%2Fmain%2Fdeployment-templates%2Fdummy%2Ftest-template.json"
-  }
+  },
+  // Add more templates here
 ];
 
 export default function Home() {
   return (
-    <div>
-      <h1>AzureDeployHub</h1>
-      <p>One-click deployment templates for Azure</p>
-      <div style={{ display: 'flex', gap: '20px' }}>
+    <div className="container">
+      <header className="text-center my-5">
+        <h1>AzureDeployHub</h1>
+        <p>One-click deployment templates for Azure</p>
+      </header>
+      
+      <div className="row">
         {templates.map((template) => (
-          <TemplateCard
-            key={template.name}
-            name={template.name}
-            description={template.description}
-            deployUrl={template.deployUrl}
-          />
+          <div className="col-md-4" key={template.name}>
+            <TemplateCard
+              name={template.name}
+              description={template.description}
+              deployUrl={template.deployUrl}
+            />
+          </div>
         ))}
       </div>
     </div>
