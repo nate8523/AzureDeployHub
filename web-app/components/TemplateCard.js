841fdbc3507ec51b8b29131fadff42f09f2be1c2
Nathan <33833899+nate8523@users.noreply.github.com>
2024-12-19 19:56:28 +0000

    Update TemplateCard.js

--- web-app/components/TemplateCard.js
+++ web-app/components/TemplateCard.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 export default function TemplateCard({ name, description, deployUrl }) {
   return (
-    <div className="card mb-4 shadow-sm">
+    <div
+      className="card text-center d-flex justify-content-center align-items-center"
+      style={{ width: '18rem', padding: '20px', margin: '10px', boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)' }}
+    >
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{description}</p>
