19a3ff950847aaa686fd5291f79d7284f2e7c8f7
Nathan <33833899+nate8523@users.noreply.github.com>
2024-12-19 14:13:49 +0000

    Update next.config.js

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-  output: 'export', // Enables static site generation
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export' // Configures Next.js for static export
 };
+
+module.exports = nextConfig;
