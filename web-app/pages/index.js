67d593f2d6ab3d53ccbb807b1764a2dd97633353
Nathan <33833899+nate8523@users.noreply.github.com>
2024-12-20 08:59:29 +0000

    Update index.js

--- web-app/pages/index.js
+++ web-app/pages/index.js
@@ -1,32 +1,43 @@
 import React from 'react';
 import TemplateCard from '../components/TemplateCard';
+import '../styles/styles.css';
 
 const templates = [
   {
     name: "Test Resource Group",
     description: "Deploy a test resource group in Azure.",
-    deployUrl: "https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fnate8523%2FAzureDeployHub%2Frefs%2Fheads%2Fmain%2Fdeployment-templates%2Fdummy%2Ftest-template.json"
+    deployUrl: "https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fnate8523%2FAzureDeployHub%2Frefs%2Fheads%2Fmain%2Fdeployment-templates%2Fdummy%2Ftest-template.json",
   },
 ];
 
 export default function Home() {
-    return (
-        <div className="container">
-            <header className="text-center my-5">
-                <h1>AzureDeployHub</h1>
-                <p>One-click deployment templates for Azure</p>
-            </header>
-            <div className="row">
-                {templates.map((template) => (
-                    <div className="col-md-4" key={template.name}>
-                        <TemplateCard
-                            name={template.name}
-                            description={template.description}
-                            deployUrl={template.deployUrl}
-                        />
-                    </div>
-                ))}
+  const toggleTheme = () => {
+    document.body.classList.toggle('light-mode');
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <h1>AzureDeployHub</h1>
+        <p>One-click deployment templates for Azure</p>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+      </header>
+      <main>
+        <div className="row">
+          {templates.map((template) => (
+            <div className="col-md-4" key={template.name}>
+              <TemplateCard
+                name={template.name}
+                description={template.description}
+                deployUrl={template.deployUrl}
+              />
             </div>
+          ))}
         </div>
-    );
+      </main>
+      <footer>
+        © 2024 AzureDeployHub - All Rights Reserved.
+      </footer>
+    </div>
+  );
 }
